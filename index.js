4451ebb4b03c3bbbffa449668f835d548c114161
mdatif796 <mdatif796@gmail.com>
2021-10-04 12:51:01 +0530

    Initial commit

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use('/', require('./routes'));
+
+
+
 app.listen(port, (err) => {
     if (err) {
         console.log(`Error on connecting port:${port}`);
