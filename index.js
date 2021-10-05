05accfddcdd7dcb4daa4ceea9fb44fc05c4232d2
mdatif796 <mdatif796@gmail.com>
2021-10-06 00:24:47 +0530

    create static files and install express-ejs-layouts

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const ejs = require('ejs');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 const port = 3000;
+
+// set up the static files like css,images
+app.use(express.static("public"));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // Use express router
 app.use('/', require('./routes'));
 // Set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
-// set up the partials
-app.use(express.static('public'));
 
 
 
