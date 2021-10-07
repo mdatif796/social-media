f2d96e9e8e3129d0828efc74348aaf3949dbe1f6
mdatif796 <mdatif796@gmail.com>
2021-10-07 11:07:35 +0530

    fix .env file and mongodb connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,19 +1,20 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const password = process.env.PASSWORD; // fetching PASSWORD from .env file
-// connecting to mongoDB 
-mongoose.connect('mongodb+srv://Admin-Atif:test123@cluster0.lymyd.mongodb.net/socialSite?retryWrites=true&w=majority');
-// mongoose.connect('mongodb+srv://Admin-Atif:'+password+'@cluster0.lymyd.mongodb.net/socialSite?retryWrites=true&w=majority');
+// connecting to mongoDB
+mongoose.connect(
+  "mongodb+srv://Admin-Atif:" +
+    password +
+    "@cluster0.lymyd.mongodb.net/socialSite?retryWrites=true&w=majority"
+);
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+db.on("error", console.error.bind(console, "Error connecting to MongoDB"));
 
-
-db.once('open', function(){
-    console.log('Connected to Database :: MongoDB');
+db.once("open", function () {
+  console.log("Connected to Database :: MongoDB");
 });
-
 
 module.exports = db;
