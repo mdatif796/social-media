7c012ccc00db4e752a43dd00e9cf8ad111eb3765
mdatif796 <mdatif796@gmail.com>
2021-10-09 12:58:23 +0530

    creating passport-local-strategy for passport using passport auth

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local');
+const User = require('../models/user');
+
+// passport use LocalStrategy for authentication
+passport.use(new LocalStrategy({   // LocalStrategy takes two param ,
+    userNameField: 'email'         // one is username
+},function(email,password,done){   // and the second one is function
+
+    // find user using email property
+    User.findOne({email:email},(err,user)=>{
+        if(err){
+            console.log('Error in finding the user in --> passport');
+            return done(err);
+        }
+        if(!user || user.password != password){
+            return done(null,false);
+        }
+        return done(null,user);
+    });
+}));
+
+
+// serializing the user which key is set as cookie
+passport.serializeUser((user,done)=>{
+    done(null,user.id);
+});
+
+// deserializing the user from the key in the cookies
+passport.deserializeUser((id,done)=>{
+    User.findById(id,(err,user)=>{
+        if(err){
+            console.log('Error in finding the user -->passport');
+            return done(err);
+        }
+        return done(null,user);
+    });
+});
