4d51c894df136d10930b0afd40aaf1866e56544d
mdatif796 <mdatif796@gmail.com>
2021-10-10 20:42:09 +0530

    add signout functionality

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ console.log('user controller');
 router.get('/profile', passport.checkAuthentication , userController.profile);
 router.get('/sign-up', userController.signUp);
 router.get('/sign-in', userController.signIn);
+router.get('/sign-out',userController.destroySession);
 
 router.post('/create', userController.create);
 
