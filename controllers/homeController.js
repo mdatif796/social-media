11e0145b1cf9694bdbac8f2cdaa915e9de9d7b90
mdatif796 <mdatif796@gmail.com>
2021-10-16 17:50:34 +0530

    find and populate user data and showing it

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,14 +3,24 @@ const Post = require("../models/post");
 module.exports.home = function (req, res) {
   // res.cookie('user_id',55);  // cookies sent to browser
   // console.log(req.cookies);  // cookies comes from browser
-  Post.find({}, (err, posts) => {
-    if (err) {
-      console.log("Error in finding the post of a user");
-      return;
-    }
-    return res.render("home", {
+  // Post.find({}, (err, posts) => {
+  //   if (err) {
+  //     console.log("Error in finding the post of a user");
+  //     return;
+  //   }
+  //   console.log(posts);
+  //   return res.render("home", {
+  //     title: "Home",
+  //     posts: posts,
+  //   });
+  // });
+
+  // Populate the data of user
+  Post.find({}).populate('user').exec((err,posts)=>{
+    if(err){console.log('Error in finding the post');return;}
+    return res.render('home',{
       title: "Home",
-      posts: posts,
+      posts: posts
     });
   });
 }
